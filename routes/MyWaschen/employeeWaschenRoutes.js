4c6a132c4ac37ccaf120bd4c9c20e39c4d598238
saputraananda <saputraananda70@gmail.com>
2026-08-13 11:25:40 +0700

    feat : mywaschen role page

---
+++ routes/MyWaschen/employeeWaschenRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { requireAuth } from "../../middleware/auth.js";
+import {
+  listWaschenEmployees,
+  getAssignableEmployees,
+  addWaschenEmployee,
+  updateEmployeeRole,
+} from "../../controllers/MyWaschen/employeeWaschenController.js";
+
+const router = express.Router();
+
+router.get("/", requireAuth, listWaschenEmployees);
+router.get("/assignable", requireAuth, getAssignableEmployees);
+router.post("/", requireAuth, addWaschenEmployee);
+router.put("/:id/role", requireAuth, updateEmployeeRole);
+
+export default router;
